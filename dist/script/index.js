8ab21cd4aecaeffc912feadba87b5f239e957bdf
Arman D Haidarpour <arman0dh@gmail.com>
2021-01-24 20:37:54 +0330

    person

--- dist/script/index.js
+++ dist/script/index.js
@@ -1,41 +1,49 @@
-// document.write("Admin");
-// console.log("admin");
-// alert("admin");
-// var _firstName = "Arman";
-// var _lastName = "Dezfuli";
-// console.log(_firstName);
-// var x = 51;
-// var y = 50;
-// if ( x > y) {
-//     console.log('yes');
-// }else if(x < y) {
-//   console.log('no');
-// }else {
-//   console.log("?");
-// }
-// function gam (x , y) {
-//   var xx;
-//   xx = (x + y) * 2;
-//   return xx;
-// }
-// var u = gam(5 , 10);
-// console.log(u);
-function sum () {
-    var m1;
-    var m2;
-    var h2;
-    m1 = document.getElementById("m1").value;
-    m2 = document.getElementById("m2").value;
-    // h2 = document.getElementById("m2").innerHTML;
-    if (Number(m1) > Number(m2)){
-        console.log("مقدار اول بزرگ تر است");
-        var q = "مقدار اول بزرگ تر است";
-        // h2.document.write(q);
+// var names = [
+//     "Arman",
+//     "Admin",
+//     "Kakashi"
+// ];
+// // names[0] = "6";
+// console.log(names);
+// var person = {
+//     name: "Arman",
+//     gmail: "arman0dh@gmail.com",
+//     age: 21,
+//     job: "Developer"
+// };
+// var show = person.gmail;
+// var show2 = person["job"] = "Front End Engineer";
+// console.log(show);
+// console.log(show2);
+var person = {
+    FirstName: "Arman",
+    lastName: "Dezfuli",
+    age: 21 ,
+    job: "Front-End Engineer",
+    skills: [
+        "HTML 5",
+        "CSS 3",
+        "JavaScript",
+        "SASS",
+        "Git",
+    ],
+    rank: function () {
+        if (skills.length >= 2) {
+            var y;
+            y = "Bronze";
+        }else if (skills.length > 4){
+            y = "Silver";
+        }else if (skills.length > 6) {
+            y = "Golden";
+        }else if (skills.length > 8) {
+            y = "Master"
+        }else if (skills.length < 2) {
+            y = "No rating";
+        }else{
+            y = "???";
+        }
+        return console.log(y);
+    },
 
-    }else if (Number(m1) < Number(m2)) {
-        console.log("مقدار دوم بزرگ تر است")
-    }
-    else{
-        console.log("هر دو مساوی هستن");
-    }
 }
+console.log(person.rank);
